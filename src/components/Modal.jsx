2f1ff6b1a8daa09017816ab9bdae1415398723dc
Vasu Kalluru <84690382+KALLURUSREENIVASULU@users.noreply.github.com>
2023-09-09 17:06:23 +0530

    first commit

---
+++ src/components/Modal.jsx
@@ -0,0 +1,30 @@
+import { createPortal } from 'react-dom';
+import { motion } from 'framer-motion';
+
+export default function Modal({ title, children, onClose }) {
+  // const hiddenAnimationState = { opacity: 0, y: 30 };
+
+  return createPortal(
+    <>
+      <div className="backdrop" onClick={onClose} />
+      <motion.dialog
+        variants={{
+          hidden: { opacity: 0, y: 30 },
+          visible: { opacity: 1, y: 0 },
+        }}
+        // initial={hiddenAnimationState}
+        initial="hidden"
+        // animate={{ opacity: 1, y: 0 }}
+        animate="visible"
+        // exit={hiddenAnimationState}
+        exit="hidden"
+        open
+        className="modal"
+      >
+        <h2>{title}</h2>
+        {children}
+      </motion.dialog>
+    </>,
+    document.getElementById('modal')
+  );
+}
